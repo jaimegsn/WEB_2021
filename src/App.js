54b5206b0b89e11c358c50a43ff7b9b4f404b7ed
Jaime Neto <jaime.jgsn@gmail.com>
2021-10-03 17:55:44 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import Jaime from './components/atividade1/index.js';
+
+function App() {
+  return (
+    <>
+      <Jaime/>
+    </>
+  );
+}
+
+export default App;
